4a4cdf786ad7ae7ba45a29d02d76ace963cb6d21
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-02-04 19:34:20 -0500

    initial commit

---
+++ run_them_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Run each of the benchmark files in turn, logging output
+
+# First, use the current date and time to generate a log file
+DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+echo "$DATE\n\n" > benchmarks_$DATE.log
+
+# Next, run each benchmark in turn, logging output
+set -x
+naive_numpy.py | tee -a benchmarks_$DATE.log
+naive_scipy.py | tee -a benchmarks_$DATE.log
+naive_r_corr.R | tee -a benchmarks_$DATE.log
+
